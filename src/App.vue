41ba83637a766a6d7ccb975fc3f508b68c18db7c
Vivek <vivek.mohit@cognizant.com>
2024-03-19 11:13:46 +0530

    Initial News app developed using copilot

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
+
+/* Create a template for showing news in a card 
+ title should be bold and small font
+ content should be displayed on hover of the card
+ card should have light grey background and black text
+*/
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>News</h1>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <div class="card">
+          <h2 class="card-title">{{ article.title }}</h2>
+          <p class="card-content">{{ article.content }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+created() {
+  this.fetchNews();
+},
+methods: {
+  // Create a function to call the news API and store the string result in a variable
+  async fetchNews() {
+    try {
+      const response = await fetch('https://localhost:7016/api/News');
+      const data = await response.json();
+      console.log(data);
+      this.articles = data.articles;
+    } catch (error) {
+      console.error(error);
+    }
   }
+},
+data() {
+  return {
+    articles: []
+  };
 }
+}
+
 </script>
 
 <style>
